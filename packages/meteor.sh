8fdbce0084d8b82feb3ecf6d68588c5fb9fca641
Florian Schaeffler <flo@hikey.io>
2016-10-26 14:29:59 +0300

    added retries for meteor install

--- packages/meteor.sh
+++ packages/meteor.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-# Install Meteor, https://www.meteor.com
-#
-# Include in your builds via
-# \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/meteor.sh | bash -s
-# 
-# Read your Meteor version via
-# export METEOR_RELEASE=$(cat .meteor/release | sed "s/METEOR@//g")
 
 METEOR_RELEASE=${METEOR_RELEASE:=""}
-\curl -sSL https://install.meteor.com | if [[ $METEOR_RELEASE ]]; then sed -e 's/^RELEASE=.*$/RELEASE="'${METEOR_RELEASE}'"/g'; else sed -e 's/ / /g'; fi | sed -e 's/PREFIX=.*/PREFIX="${HOME}"/g' | sh
+METEOR_INSTALL_RETRIES=${METEOR_INSTALL_RETRIES:="3"}
+
+install_retries="0"
+
+try_install () {
+	tmpfile=$(mktemp /tmp/abc-script.XXXXXX)
+	curl -sSL https://install.meteor.com -o $tmpfile
+
+	check_503=$(cat $tmpfile | grep '503 Service Unavailable')
+	if [[ $check_503 == '' ]]; then
+		if [[ $METEOR_RELEASE ]]; then
+			sed -i 's/^RELEASE=.*$/RELEASE="'${METEOR_RELEASE}'"/g' $tmpfile;
+		fi
+
+		sed -i 's/PREFIX=.*/PREFIX="${HOME}"/g' $tmpfile
+
+		chmod +x $tmpfile
+		$tmpfile
+
+		rm $tmpfile
+	elif [[ "$install_retries" -gt "$METEOR_INSTALL_RETRIES" ]];
+		echo "installation failed"
+		rm $tmpfile
+		exit 1
+	else
+		rm $tmpfile
+		install_retries=$(($install_retries + 1))
+	fi
+}
+
+try_install
+
+unset try_install
