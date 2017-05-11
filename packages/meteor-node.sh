d97376f17af1f01af33a00745e6a4ef0644ccc2a
Florian Schaeffler <flo@hikey.io>
2017-05-11 17:34:31 +0300

    stashed

--- packages/meteor-node.sh
+++ packages/meteor-node.sh
@@ -7,8 +7,13 @@ meteor_minor=$(echo $test | cut -d'.' -f2)
 
 node_version="0.10.46"
 
+echo "$meteor_major"
+echo "$meteor_minor"
+
 if [[ "$meteor_major" -ge "1" ]]; then
+	echo "test"
 	if [[ "$meteor_minor" -ge "3" ]]; then
+		echo "test2s"
 		node_version="4.6.1"
 	fi
 fi
